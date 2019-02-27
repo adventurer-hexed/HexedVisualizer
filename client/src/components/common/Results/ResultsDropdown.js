7905031b374f162ced8df8b0e84588fbd418fa45
Anthony Rivas <kamikazebot@gmail.com>
2019-02-26 22:16:28 -0700

    Updating player styling to be self contained and adding search result styling

--- client/src/components/common/Results/ResultsDropdown.js
+++ client/src/components/common/Results/ResultsDropdown.js
@@ -1,42 +1,48 @@
 import React, { Component } from "react"
+import "./ResultsDropdown.css"
 
-class ResultsDropdown extends Component{
-    generateCards = ()=>{
-        const {results, playSong} = this.props
+class ResultsDropdown extends Component {
+    generateCards = () => {
+        const { results, playSong } = this.props
 
-        return results.tracks.items.map((song, i)=>{
-            return(
-                <li className="resultItem" 
+        return results.tracks.items.map((song, i) => {
+            return (
+                <li className="resultItem"
                     key={i}
                     onClick={
-                        (event)=>{
+                        (event) => {
                             console.log(song.uri)
                             playSong(song.uri)
                         }
-                }>
-                    <div className="songName">
-                        Title: {song.name}
+                    }>
+                    <div className="songImage">
+                        <img src={song.album.images[2].url} alt="Album Cover" />
                     </div>
-                    <div className="songArtist">
-                        Artist: {song.artists.reduce((out, artist)=>{
-                            if(out === ""){
-                                return artist.name
-                            }
-                            return out += `, ${artist.name}`
-                        }, "")}
+                    <div className="songInfo">
+                        <div className="songName">
+                            Title: {song.name}
+                        </div>
+                        <div className="songArtist">
+                            Artist: {song.artists.reduce((out, artist) => {
+                                if (out === "") {
+                                    return artist.name
+                                }
+                                return out += `, ${artist.name}`
+                            }, "")}
+                        </div>
                     </div>
                 </li>
             )
         })
     }
-    render(){
+    render() {
         let { results } = this.props
-        return(
-            (results.tracks)?
-            <ul className="searchResults">
-                {this.generateCards()}
-            </ul>
-            : ""
+        return (
+            (results.tracks) ?
+                <ul className="searchResults">
+                    {this.generateCards()}
+                </ul>
+                : ""
         )
     }
 }
