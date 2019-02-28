58f08425e05d1bdadbacc9f12a8ca8583984a0ab
Anthony Rivas <kamikazebot@gmail.com>
2019-02-28 13:09:52 -0700

    Fixing problem with deviceid not populating on login

--- client/src/components/common/SpotifyScript.js
+++ client/src/components/common/SpotifyScript.js
@@ -3,53 +3,54 @@ import Script from "react-load-script"
 import { connect } from "react-redux"
 import { deviceStateListener, fetchAvailableDevices, fetchAnalysis, updateCurrentDeviceId } from "../../actions"
 
-export default connect(null, { deviceStateListener, fetchAvailableDevices, fetchAnalysis, updateCurrentDeviceId})(
+export default connect(null, { deviceStateListener, fetchAvailableDevices, fetchAnalysis, updateCurrentDeviceId })(
 
     class SpotifyScript extends React.Component {
-        handleScriptLoad = () => {
+        setupScriptLoad = () => {
             window.onSpotifyWebPlaybackSDKReady = () => {
                 const player = new window.Spotify.Player({
-                name: "Visualizer",
-                getOAuthToken: cb => { cb(this.props.token); }
+                    name: "Visualizer",
+                    getOAuthToken: cb => { cb(this.props.token); }
                 });
-        
+
                 // Error handling
                 player.addListener('initialization_error', ({ message }) => { console.error(message); });
                 player.addListener('authentication_error', ({ message }) => { console.error(message); });
                 player.addListener('account_error', ({ message }) => { console.error(message); });
                 player.addListener('playback_error', ({ message }) => { console.error(message); });
-        
+
                 // Playback status updates
                 player.addListener('player_state_changed', state => {
                     console.log(state);
-                    if(state) {
+                    if (state) {
                         this.props.deviceStateListener(state.paused)
                     }
                     this.props.fetchAnalysis(state.track_window.current_track.id)
-                    this.props.fetchAvailableDevices()               
+                    this.props.fetchAvailableDevices()
                 });
-        
+
                 // Ready
                 player.addListener('ready', ({ device_id }) => {
                     console.log(device_id)
                     this.props.updateCurrentDeviceId(device_id)
                 });
-        
+
                 // Not Ready
                 player.addListener('not_ready', ({ device_id }) => {
                     console.log('Device ID has gone offline', device_id);
                 });
-        
+
                 // Connect to the player!
                 player.connect();
             };
         }
-
+        componentWillMount() {
+            this.setupScriptLoad();
+        }
         render() {
             return (
                 <Script
                     url="https://sdk.scdn.co/spotify-player.js"
-                    onLoad={this.handleScriptLoad}
                 />
             )
         }
