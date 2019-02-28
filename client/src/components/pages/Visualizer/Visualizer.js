73e2c9399a2001e4d62c2cbb581bf0d83219e15b
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2019-02-27 23:32:34 -0700

    spotify refreshes token when token is out of date

--- client/src/components/pages/Visualizer/Visualizer.js
+++ client/src/components/pages/Visualizer/Visualizer.js
@@ -4,14 +4,14 @@ import music from "./music.json"
 import { connect } from "react-redux"
 
 class Visualizer extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
-            startingTime : null,
             lastTime :null,
             totalElapsedTime :null
         }
         this._canvas = React.createRef()
+        this._startingTime = props.progress;
     }
 
     componentDidMount() {
@@ -26,7 +26,7 @@ class Visualizer extends React.Component {
     }
     
     componentWillUnmount() {
-        window.cancelAnimationFrame(this.animationFrame)
+        window.cancelAnimationFrame(this._animationFrame)
         window.removeEventListener("resize", this.handleWindowResize)
     }
 
@@ -38,16 +38,17 @@ class Visualizer extends React.Component {
         this._completeCircle._canvasWidth = this._canvas.current.width
     }
 
-    animate(currentTime) {
-        if(!this.state.startingTime) this.state.startingTime = currentTime;
-        if(!this.state.lastTime) this.state.lastTime=currentTime += 20
-        this.state.totalElapsedTime = (currentTime - this.state.startingTime);
-        this.animationFrame = requestAnimationFrame(this.animate.bind(this))
+    animate = (currentTime) => {
+        if(!this._startingTime) this._startingTime = currentTime;
+        if(!this.state.lastTime) this.state.lastTime = currentTime
+
+        this.state.totalElapsedTime = (currentTime - this._startingTime);
+        this._animationFrame = requestAnimationFrame(this.animate.bind(this))
         this._ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+
+        
         this._completeCircle.update(this.state.totalElapsedTime, music.beats, music.tatums)
         
-
-
 
         // requestAnimationFrame(this.animate.bind(this))
         // this._ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
