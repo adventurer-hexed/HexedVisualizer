71b503710accfe08b67e2f9fde920cf644d30234
Anthony Rivas <kamikazebot@gmail.com>
2019-03-17 11:23:51 -0700

    Merging

--- client/src/components/pages/Visualizer/vis_one/RippleCircle.js
+++ client/src/components/pages/Visualizer/vis_one/RippleCircle.js
@@ -1,14 +1,14 @@
-import Circle from './Circle';
+import Circle from "./Circle";
 // rgb(250,250,250)
 // rgb(0,212,103)
 // rgb(234,92,144)
 // rgb(25,176,221)
-const colorOpacity = opacity => [
-  `rgba(250,250,250, ${opacity})`, // Grey
-  `rgba(0,212,103, ${opacity})`, // Green
-  `rgba(234,92,144, ${opacity})`, // Pinkish Red
-  `rgba(25,176,221,${opacity})`, // Light Blue
-];
+const colorOpacity = (opacity) => [
+    `rgba(250,250,250, ${opacity})`, // Grey
+    `rgba(0,212,103, ${opacity})`, // Green
+    `rgba(234,92,144, ${opacity})`, // Pinkish Red
+    `rgba(25,176,221,${opacity})` // Light Blue
+]
 
 export default class RippleCircle extends Circle {
     constructor(ctx, x, y, rad, dRad, color, isFill, opacity, confidence, dOpacity) {
@@ -20,6 +20,18 @@ export default class RippleCircle extends Circle {
       this._dRad = dRad
       this._dOpacity = dOpacity
     }
+      draw() {
+          this._ctx.beginPath()
+          let color;
+          if(this._confidence > .80) {
+            color = colorOpacity(this._opacity)[1]
+          } else if(this._confidence > .50) {
+            color = colorOpacity(this._opacity)[2]
+          } else if(this._confidence > .4) {
+            color = colorOpacity(this._opacity)[3]
+          } else {
+            color = colorOpacity(this._opacity)[0]
+          }
 
           this._ctx.strokeStyle = color
           
@@ -56,14 +68,5 @@ export default class RippleCircle extends Circle {
       }
 }
 
-    if (this._isFill) {
-      this._ctx.fillStyle = color;
-    }
-    this._ctx.lineWidth = 10;
-    this._ctx.arc(this._x, this._y, this._rad, 0, Math.PI * 2, false);
-    this._ctx.stroke();
-    if (this._isFill) {
-      this._ctx.fill();
-    }
-  }
+
 
