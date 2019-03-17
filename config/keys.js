3948f983899c7c6a7131dc8f60d3229f34c7abf1
Anthony Rivas <kamikazebot@gmail.com>
2019-03-17 12:56:41 -0700

    Fixing bug that causes app to crash in production

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
-const PROD = require('./prod');
-const DEV = require('./dev');
-
 if (process.env.NODE_ENV === 'production') {
+  const PROD = require('./prod');
   module.exports = PROD;
 } else {
+  const DEV = require('./dev');
   module.exports = DEV;
 }
